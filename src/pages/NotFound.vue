2330dce50403b82256476105f3d5b777b07a9e3c
LuHuan <ariellushanghai@icloud.com>
2018-04-17 21:44:37 +0800

    MODIFY: NotFound behaviour

--- src/pages/NotFound.vue
+++ src/pages/NotFound.vue
@@ -1,28 +1,28 @@
 <template lang="pug">
     .bg
-        span 页面没找到，即将返回首页。。。
+        div 页面没找到,
+            router-link(:to="{name: 'index'}", :replace="true") 返回首页
 </template>
 
 <script>
 
-    export default {
-        name: 'NotFound',
-        metaInfo: {
-            titleTemplate: '%s-404'
-        },
-        mounted() {
-            return setTimeout(_ => {
-                this.$router.replace({name: 'index'});
-            }, 3000);
-        }
+  export default {
+    name: 'NotFound',
+    metaInfo: {
+      titleTemplate: '%s-404'
     }
+  }
 </script>
 
 <style lang="stylus" scoped>
-    ping_an-orange = #EA5505
     .bg
-        background-color ping_an-orange
-        color white
+        position fixed
+        top 0
+        left 0
+        right 0
+        bottom 0
+        color #303133
+        background-color white
         width 100%
         height 100%
         display flex
